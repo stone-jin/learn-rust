8dbb0ba990e42eb35e3d414c145122c59ca20f5e
stone-jin <1520006273@qq.com>
2021-12-24 00:25:53 +0800

    chore: add tokio redis client & server

--- hello_tokio/src/main.rs
+++ hello_tokio/src/main.rs
@@ -1,16 +1,34 @@
-use mini_redis::{Result, client};
+use mini_redis::Frame;
+use mini_redis::Connection;
+use tokio::net::{TcpListener, TcpStream};
+use tokio::spawn;
+use mini_redis::{Result};
+
+async fn process(tcpstream: TcpStream){
+  let mut connection = Connection::new(tcpstream);
+  if let Some(frame) = connection.read_frame().await.unwrap(){
+    println!("Got: {:?}", frame);
+    let response = Frame::Error(String::from("not implments"));
+    connection.write_frame(&response).await.unwrap();
+  }
+}
+
+async fn start_server(){
+  let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+  loop{
+    let (tcpstream, address) = listener.accept().await.unwrap();
+    println!("address: {}", address);
+    spawn(async move {
+      process(tcpstream).await;
+    });
+  }
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    // Open a connection to the mini-redis address.
-    let mut client = client::connect("127.0.0.1:6379").await?;
 
-    // Set the key "hello" with value "world"
-    client.set("hello", "world".into()).await?;
+    println!("in server");
 
-    // Get key "hello"
-    let result = client.get("hello").await?;
-
-    println!("got value from the server; result={:?}", result);
+    start_server().await;
     Ok(())
 }
