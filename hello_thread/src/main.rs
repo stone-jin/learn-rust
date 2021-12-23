37a15ba0aa3d1efac39cbbba0892967da093a94c
stone-jin <1520006273@qq.com>
2021-12-23 23:10:25 +0800

    chore: thread join, vec move

--- hello_thread/src/main.rs
+++ hello_thread/src/main.rs
@@ -1,7 +1,13 @@
+use std::time::Duration;
+use std::thread;
 
 fn main() {
+  let v1 = vec![1,2,3];
   let jh = std::thread::spawn(move || {
-    println!("in thead ");
+     for i in 1..10 {
+            println!("hi number {} from the spawned thread! {:?}", i, v1);
+            thread::sleep(Duration::from_millis(1));
+        }
   });
   println!("before main thread.");
   jh.join().unwrap();
