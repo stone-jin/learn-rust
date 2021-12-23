7a7ffc3cd0de68d9cd7e453476904e7b2150b295
stone-jin <1520006273@qq.com>
2021-12-23 23:26:03 +0800

    chore: more thread mpsc

--- hello_thread/src/main.rs
+++ hello_thread/src/main.rs
@@ -1,7 +1,8 @@
 use std::time::Duration;
 use std::thread;
+use std::sync::mpsc;
 
-fn main() {
+fn demo1(){
   let v1 = vec![1,2,3];
   let jh = std::thread::spawn(move || {
      for i in 1..10 {
@@ -12,4 +13,72 @@ fn main() {
   println!("before main thread.");
   jh.join().unwrap();
   println!("after main thread.");
+}
+
+fn demo_mpsc(){
+  let (tx, rx) = mpsc::channel();
+  std::thread::spawn(move || {
+    tx.send("hello mpsc").unwrap();
+  });
+  let result = rx.recv().unwrap();
+  println!("result: {}", result);
+}
+
+fn demo_mpsc_mul_msg(){
+  let (tx, rx) = mpsc::channel();
+  std::thread::spawn(move || {
+    let msgs = vec![
+      String::from("msg1"),
+      String::from("msg2"),
+      String::from("msg3"),
+    ];
+
+    for msg in msgs{
+      tx.send(msg).unwrap();
+    }
+  });
+  for msg in rx{
+    println!("rec: {}", msg);
+  }
+}
+
+fn demo_mpsc_mul_tx(){
+  let (tx, rx) = mpsc::channel();
+  let tx1 = tx.clone();
+  std::thread::spawn(move || {
+    let msgs = vec![
+      String::from("tx_msg1"),
+      String::from("tx_msg2"),
+      String::from("tx_msg3"),
+    ];
+
+    for msg in msgs{
+      tx.send(msg).unwrap();
+      thread::sleep(Duration::from_secs(1));
+    }
+  });
+
+  std::thread::spawn(move || {
+    let msgs = vec![
+      String::from("tx1_msg1"),
+      String::from("tx1_msg2"),
+      String::from("tx1_msg3"),
+    ];
+
+    for msg in msgs{
+      tx1.send(msg).unwrap();
+      thread::sleep(Duration::from_secs(1));
+    }
+  });
+
+  for msg in rx{
+    println!("rec: {}", msg);
+  }
+} 
+
+fn main() {
+  demo1();
+  demo_mpsc();
+  demo_mpsc_mul_msg();
+  demo_mpsc_mul_tx();
 }
