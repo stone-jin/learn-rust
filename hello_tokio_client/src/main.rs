3f96694dbbfd59fdf114c8d1c836e90277471296
stone-jin <1520006273@qq.com>
2021-12-26 11:23:45 +0800

    chore: client send by tokio's mpsc

--- hello_tokio_client/src/main.rs
+++ hello_tokio_client/src/main.rs
@@ -1,18 +1,63 @@
 
 use mini_redis::{Result, client};
+use bytes::Bytes;
+use tokio::sync::mpsc;
+
+#[derive(Debug)]
+enum Command {
+    Get {
+      key: String,
+    },
+    Set{
+      key: String,
+      val: Bytes,
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    // Open a connection to the mini-redis address.
-    let mut client = client::connect("127.0.0.1:6379").await?;
+    let (tx, mut rx) = mpsc::channel(32);
 
-    // Set the key "hello" with value "world"
-    client.set("hello", "world".into()).await?;
+    let tx2 = tx.clone();
 
-    // Get key "hello"
-    let result = client.get("hello").await?;
+    // Spawn two tasks, one gets a key, the other sets a key
+    let t1 = tokio::spawn(async move {
+        let cmd = Command::Get {
+            key: "foo".to_string(),
+        };
 
-    println!("got value from the server; result={:?}", result);
+        tx.send(cmd).await.unwrap();
+    });
+
+    let t2 = tokio::spawn(async move {
+        let cmd = Command::Set {
+            key: "foo".to_string(),
+            val: "bar".into(),
+        };
+
+        tx2.send(cmd).await.unwrap();
+    });
+
+    let manager = tokio::spawn(async move {
+      let mut client = client::connect("127.0.0.1:6379").await.unwrap();
+      while let Some(cmd) = rx.recv().await {
+        use Command::*;
+        match cmd {
+            Get { key } => {
+                let res = client.get(&key).await;
+                println!("res: {:?}", res);
+            }
+            Set { key, val } => {
+                let _ = client.set(&key, val).await;
+            }
+        }
+      }
+    });
+
+    t1.await.unwrap();
+    t2.await.unwrap();
+    manager.await.unwrap();
+    // println!("got value from the server; result={:?}", result);
 
     // jh.join();
     Ok(())
