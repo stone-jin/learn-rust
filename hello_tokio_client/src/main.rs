61e62cb2ad2e76703e71278a97972efaa485cdc2
stone-jin <1520006273@qq.com>
2021-12-26 11:38:57 +0800

    chore: use tokei::sync::oneshot

--- hello_tokio_client/src/main.rs
+++ hello_tokio_client/src/main.rs
@@ -1,16 +1,20 @@
 
 use mini_redis::{Result, client};
 use bytes::Bytes;
-use tokio::sync::mpsc;
+use tokio::sync::{mpsc, oneshot};
+
+type Responder<T> = oneshot::Sender<Result<T>>;
 
 #[derive(Debug)]
 enum Command {
     Get {
       key: String,
+      resp: Responder<Option<Bytes>>
     },
     Set{
       key: String,
       val: Bytes,
+      resp: Responder<()>
     }
 }
 
@@ -22,20 +26,30 @@ async fn main() -> Result<()> {
 
     // Spawn two tasks, one gets a key, the other sets a key
     let t1 = tokio::spawn(async move {
+        let (resp_tx, resp_rx) = oneshot::channel();
         let cmd = Command::Get {
             key: "foo".to_string(),
+            resp: resp_tx
         };
 
         tx.send(cmd).await.unwrap();
+
+        let res = resp_rx.await;
+        println!("res: {:?}", res);
     });
 
     let t2 = tokio::spawn(async move {
+        let (resp_tx, resp_rx ) = oneshot::channel();
         let cmd = Command::Set {
             key: "foo".to_string(),
             val: "bar".into(),
+            resp: resp_tx
         };
 
         tx2.send(cmd).await.unwrap();
+
+        let res = resp_rx.await;
+        println!("set resp: {:?}", res);
     });
 
     let manager = tokio::spawn(async move {
@@ -43,12 +57,13 @@ async fn main() -> Result<()> {
       while let Some(cmd) = rx.recv().await {
         use Command::*;
         match cmd {
-            Get { key } => {
+            Get { key, resp } => {
                 let res = client.get(&key).await;
-                println!("res: {:?}", res);
+                let _ = resp.send(res).unwrap();
             }
-            Set { key, val } => {
-                let _ = client.set(&key, val).await;
+            Set { key, val, resp } => {
+                let res = client.set(&key, val).await;
+                let _ = resp.send(res).unwrap();
             }
         }
       }
