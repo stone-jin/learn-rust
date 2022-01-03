d6a0de818bac0b7a10da69f7dc76b9b3385c9203
stone-jin <1520006273@qq.com>
2022-01-03 13:31:26 +0800

    chore: format rust

--- hello_tokio/src/main.rs
+++ hello_tokio/src/main.rs
@@ -1,53 +1,53 @@
-use mini_redis::Frame;
+use bytes::Bytes;
+use mini_redis::Command::{self, Get, Set};
 use mini_redis::Connection;
+use mini_redis::Frame;
+use mini_redis::Result;
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 use tokio::net::{TcpListener, TcpStream};
 use tokio::spawn;
-use mini_redis::{Result};
-use std::collections::HashMap;
-use mini_redis::Command::{self, Get, Set};
-use std::sync::{Arc, Mutex};
-use bytes::Bytes;
 
 type Db = Arc<Mutex<HashMap<String, Bytes>>>;
 
-async fn process(tcpstream: TcpStream, db: Db){
-    // Connection, provided by `mini-redis`, handles parsing frames from
-    // the socket
-    let mut connection = Connection::new(tcpstream);
+async fn process(tcpstream: TcpStream, db: Db) {
+  // Connection, provided by `mini-redis`, handles parsing frames from
+  // the socket
+  let mut connection = Connection::new(tcpstream);
 
-    // Use `read_frame` to receive a command from the connection.
-    while let Some(frame) = connection.read_frame().await.unwrap() {
-        let response = match Command::from_frame(frame).unwrap() {
-            Set(cmd) => {
-                let mut db = db.lock().unwrap();
-                // The value is stored as `Vec<u8>`
-                db.insert(cmd.key().to_string(), cmd.value().clone());
-                Frame::Simple("OK".to_string())
-            }
-            Get(cmd) => {
-                let db = db.lock().unwrap();
-                if let Some(value) = db.get(cmd.key()) {
-                    // `Frame::Bulk` expects data to be of type `Bytes`. This
-                    // type will be covered later in the tutorial. For now,
-                    // `&Vec<u8>` is converted to `Bytes` using `into()`.
-                    Frame::Bulk(value.clone().into())
-                } else {
-                    Frame::Null
-                }
-            }
-            cmd => panic!("unimplemented {:?}", cmd),
-        };
+  // Use `read_frame` to receive a command from the connection.
+  while let Some(frame) = connection.read_frame().await.unwrap() {
+    let response = match Command::from_frame(frame).unwrap() {
+      Set(cmd) => {
+        let mut db = db.lock().unwrap();
+        // The value is stored as `Vec<u8>`
+        db.insert(cmd.key().to_string(), cmd.value().clone());
+        Frame::Simple("OK".to_string())
+      }
+      Get(cmd) => {
+        let db = db.lock().unwrap();
+        if let Some(value) = db.get(cmd.key()) {
+          // `Frame::Bulk` expects data to be of type `Bytes`. This
+          // type will be covered later in the tutorial. For now,
+          // `&Vec<u8>` is converted to `Bytes` using `into()`.
+          Frame::Bulk(value.clone().into())
+        } else {
+          Frame::Null
+        }
+      }
+      cmd => panic!("unimplemented {:?}", cmd),
+    };
 
-        // Write the response to the client
-        connection.write_frame(&response).await.unwrap();
-    }
+    // Write the response to the client
+    connection.write_frame(&response).await.unwrap();
+  }
 }
 
-async fn start_server(){
+async fn start_server() {
   let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
   let db = Arc::new(Mutex::new(HashMap::new()));
-  loop{
+  loop {
     let (tcpstream, address) = listener.accept().await.unwrap();
     println!("address: {}", address);
     let db = db.clone();
@@ -59,23 +59,22 @@ async fn start_server(){
 
 #[tokio::main]
 async fn main() -> Result<()> {
-
-    println!("hello_tokio start.");
-
-    // match tokio::signal::ctrl_c().await{
-    //   Ok(())=>{
-    //     println!("ctrl + c");
-    //     eprintln!("stop by ctrl+C");
-    //     std::process::exit(0);
-    //   },
-    //   Err(err)=>{
-    //     eprintln!("Unable to listen for shutdown signal: {}", err);
-    //   }
-    // }
-
-    println!("in server");
-
+  tokio::spawn(async move {
     start_server().await;
+  });
 
-    Ok(())
+  println!("in server");
+
+  match tokio::signal::ctrl_c().await {
+    Ok(()) => {
+      println!("ctrl + c");
+      eprintln!("stop by ctrl+C");
+      std::process::exit(0);
+    }
+    Err(err) => {
+      eprintln!("Unable to listen for shutdown signal: {}", err);
+    }
+  }
+
+  Ok(())
 }
--- hello_tokio_client/src/main.rs
+++ hello_tokio_client/src/main.rs
@@ -1,181 +1,179 @@
-
-use tokio::io::AsyncReadExt;
 use bytes::Buf;
-use std::io::Cursor;
-use mini_redis::Frame;
-use bytes::BytesMut;
-use tokio::net::TcpStream;
-use mini_redis::{Result, client};
 use bytes::Bytes;
-use tokio::sync::{mpsc, oneshot};
+use bytes::BytesMut;
+use mini_redis::Frame;
+use mini_redis::{client, Result};
+use std::io::Cursor;
+use tokio::io::AsyncReadExt;
 use tokio::io::AsyncWriteExt;
+use tokio::net::TcpStream;
+use tokio::sync::{mpsc, oneshot};
 
 type Responder<T> = oneshot::Sender<Result<T>>;
 
-struct Connection{
+struct Connection {
   stream: TcpStream,
-  buffer: BytesMut
+  buffer: BytesMut,
 }
 
 impl Connection {
-    pub fn new(stream: TcpStream) -> Connection{
-      Connection{
-        stream,
-        buffer: BytesMut::with_capacity(4096),
-      }
+  pub fn new(stream: TcpStream) -> Connection {
+    Connection {
+      stream,
+      buffer: BytesMut::with_capacity(4096),
     }
-    pub async fn write_frame(&mut self, frame: &Frame) -> tokio::io::Result<()>{
-      match frame{
-        Frame::Simple(val) => {
-          self.stream.write_u8(b'+').await?;
-          self.stream.write_all(val.as_bytes()).await?;
-          self.stream.write_all(b"\r\n").await?;
-        }
-        Frame::Error(val) => {
-          self.stream.write_u8(b'-').await?;
-          self.stream.write_all(val.as_bytes()).await?;
-          self.stream.write_all(b"\r\n").await?;
-        }
-        Frame::Integer(val) =>{
-          self.stream.write_u8(b':').await?;
-          self.write_decimal(*val).await?;
-        }
-        Frame::Bulk(val) =>{
-          let len = val.len();
-          self.stream.write_u8(b'$').await?;
-          self.write_decimal(len as u64).await?;
-          self.stream.write_all(val).await?;
-          self.stream.write_all(b"\r\n").await?;
-        }
-        Frame::Null => {
-          self.stream.write_all(b"$-1\r\n").await?;
-        }
-        Frame::Array(_val) => {
-          unimplemented!();
-        }
+  }
+  pub async fn write_frame(&mut self, frame: &Frame) -> tokio::io::Result<()> {
+    match frame {
+      Frame::Simple(val) => {
+        self.stream.write_u8(b'+').await?;
+        self.stream.write_all(val.as_bytes()).await?;
+        self.stream.write_all(b"\r\n").await?;
       }
-
-      self.stream.flush().await;
-
-      Ok({})
-    }
-
-    pub async fn read_frame(&mut self) -> Result<Option<Frame>>{
-      loop{
-        if let Some(frame) = self.parse_frame()? {
-          return Ok(Some(frame));
-        }
-
-        if 0 == self.stream.read_buf(&mut self.buffer).await?{
-          if self.buffer.is_empty(){
-            return Ok(None);
-          }else{
-            return Err("connection peer by peer".into());
-          }
-        }
+      Frame::Error(val) => {
+        self.stream.write_u8(b'-').await?;
+        self.stream.write_all(val.as_bytes()).await?;
+        self.stream.write_all(b"\r\n").await?;
+      }
+      Frame::Integer(val) => {
+        self.stream.write_u8(b':').await?;
+        self.write_decimal(*val).await?;
+      }
+      Frame::Bulk(val) => {
+        let len = val.len();
+        self.stream.write_u8(b'$').await?;
+        self.write_decimal(len as u64).await?;
+        self.stream.write_all(val).await?;
+        self.stream.write_all(b"\r\n").await?;
+      }
+      Frame::Null => {
+        self.stream.write_all(b"$-1\r\n").await?;
+      }
+      Frame::Array(_val) => {
+        unimplemented!();
       }
     }
 
-    pub async fn write_decimal(&mut self, val: u64)-> tokio::io::Result<()>{
-      let mut buf = [0u8; 12];
-      let mut buf = Cursor::new(&mut buf[..]);
-      use std::io::Write;
-      write!(&mut buf, "{}", val)?;
+    self.stream.flush().await;
 
+    Ok({})
+  }
 
-      let pos = buf.position() as usize;
-      self.stream.write_all(&buf.get_ref()[..pos]).await?;
-      self.stream.write_all(b"\r\n").await?;
-      Ok({})
-    }
+  pub async fn read_frame(&mut self) -> Result<Option<Frame>> {
+    loop {
+      if let Some(frame) = self.parse_frame()? {
+        return Ok(Some(frame));
+      }
 
-    pub fn parse_frame(&mut self) -> Result<Option<Frame>> {
-      let mut buf = Cursor::new(&self.buffer[..]);
-      
-      match Frame::check(&mut buf){
-        Ok(_) =>{
-          let len = buf.position() as usize;
-          buf.set_position(0);
-
-          let frame = Frame::parse(&mut buf)?;
-          self.buffer.advance(len);
-          Ok(Some(frame))
+      if 0 == self.stream.read_buf(&mut self.buffer).await? {
+        if self.buffer.is_empty() {
+          return Ok(None);
+        } else {
+          return Err("connection peer by peer".into());
         }
-        Err(Incompute) =>Ok(None),
-        Err(e)=>Err(e.into())
       }
     }
+  }
+
+  pub async fn write_decimal(&mut self, val: u64) -> tokio::io::Result<()> {
+    let mut buf = [0u8; 12];
+    let mut buf = Cursor::new(&mut buf[..]);
+    use std::io::Write;
+    write!(&mut buf, "{}", val)?;
+
+    let pos = buf.position() as usize;
+    self.stream.write_all(&buf.get_ref()[..pos]).await?;
+    self.stream.write_all(b"\r\n").await?;
+    Ok({})
+  }
+
+  pub fn parse_frame(&mut self) -> Result<Option<Frame>> {
+    let mut buf = Cursor::new(&self.buffer[..]);
+
+    match Frame::check(&mut buf) {
+      Ok(_) => {
+        let len = buf.position() as usize;
+        buf.set_position(0);
+
+        let frame = Frame::parse(&mut buf)?;
+        self.buffer.advance(len);
+        Ok(Some(frame))
+      }
+      Err(Incompute) => Ok(None),
+      Err(e) => Err(e.into()),
+    }
+  }
 }
 
 #[derive(Debug)]
 enum Command {
-    Get {
-      key: String,
-      resp: Responder<Option<Bytes>>
-    },
-    Set{
-      key: String,
-      val: Bytes,
-      resp: Responder<()>
-    }
+  Get {
+    key: String,
+    resp: Responder<Option<Bytes>>,
+  },
+  Set {
+    key: String,
+    val: Bytes,
+    resp: Responder<()>,
+  },
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let (tx, mut rx) = mpsc::channel(32);
+  let (tx, mut rx) = mpsc::channel(32);
 
-    let tx2 = tx.clone();
+  let tx2 = tx.clone();
 
-    // Spawn two tasks, one gets a key, the other sets a key
-    let t1 = tokio::spawn(async move {
-        let (resp_tx, resp_rx) = oneshot::channel();
-        let cmd = Command::Get {
-            key: "foo".to_string(),
-            resp: resp_tx
-        };
+  // Spawn two tasks, one gets a key, the other sets a key
+  let t1 = tokio::spawn(async move {
+    let (resp_tx, resp_rx) = oneshot::channel();
+    let cmd = Command::Get {
+      key: "foo".to_string(),
+      resp: resp_tx,
+    };
 
-        tx.send(cmd).await.unwrap();
+    tx.send(cmd).await.unwrap();
 
-        let res = resp_rx.await;
-        println!("res: {:?}", res);
-    });
+    let res = resp_rx.await;
+    println!("res: {:?}", res);
+  });
 
-    let t2 = tokio::spawn(async move {
-        let (resp_tx, resp_rx ) = oneshot::channel();
-        let cmd = Command::Set {
-            key: "foo".to_string(),
-            val: "bar".into(),
-            resp: resp_tx
-        };
+  let t2 = tokio::spawn(async move {
+    let (resp_tx, resp_rx) = oneshot::channel();
+    let cmd = Command::Set {
+      key: "foo".to_string(),
+      val: "bar".into(),
+      resp: resp_tx,
+    };
 
-        tx2.send(cmd).await.unwrap();
+    tx2.send(cmd).await.unwrap();
 
-        let res = resp_rx.await;
-        println!("set resp: {:?}", res);
-    });
+    let res = resp_rx.await;
+    println!("set resp: {:?}", res);
+  });
 
-    let manager = tokio::spawn(async move {
-      let mut client = client::connect("127.0.0.1:6379").await.unwrap();
-      while let Some(cmd) = rx.recv().await {
-        use Command::*;
-        match cmd {
-            Get { key, resp } => {
-                let res = client.get(&key).await;
-                let _ = resp.send(res).unwrap();
-            }
-            Set { key, val, resp } => {
-                let res = client.set(&key, val).await;
-                let _ = resp.send(res).unwrap();
-            }
+  let manager = tokio::spawn(async move {
+    let mut client = client::connect("127.0.0.1:6379").await.unwrap();
+    while let Some(cmd) = rx.recv().await {
+      use Command::*;
+      match cmd {
+        Get { key, resp } => {
+          let res = client.get(&key).await;
+          let _ = resp.send(res).unwrap();
+        }
+        Set { key, val, resp } => {
+          let res = client.set(&key, val).await;
+          let _ = resp.send(res).unwrap();
         }
       }
-    });
+    }
+  });
 
-    t1.await.unwrap();
-    t2.await.unwrap();
-    manager.await.unwrap();
-    // println!("got value from the server; result={:?}", result);
+  t1.await.unwrap();
+  t2.await.unwrap();
+  manager.await.unwrap();
+  // println!("got value from the server; result={:?}", result);
 
-    // jh.join();
-    Ok(())
+  // jh.join();
+  Ok(())
 }
